d0a0f53be14a39446b3908a20edf064fd638093b
Ruslana Logosha <ruslanalogosha@gmail.com>
2020-12-07 23:19:39 +0200

    adds Modal, IconButton,  vital cycle

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import shortid from 'shortid';
 import TodoList from './components/TodoList/todoList';
 import TodoEditor from './components/TodoEditor/todoEditor';
 import initialTodos from './components/TodoList/todos.json';
-import Filter from './components/Filter';
+import TodoFilter from './components/TodoFilter/todoFilter';
 import Container from './components/Container/container';
+import Modal from './components/Modal/modal';
+import IconButton from './components/IconButton/iconButton';
+import { ReactComponent as AddIcon } from './icons/add.svg';
 
 
 
@@ -13,7 +16,35 @@ class App extends Component {
   state = {
     todos: initialTodos,
     filter: '',
+    showModal: false,
   };
+
+  componentDidMount() {
+    // console.log('App componentDidMount');
+
+    const todos = localStorage.getItem('todos');
+    const parsedTodos = JSON.parse(todos);
+
+    if (parsedTodos) {
+      this.setState({ todos: parsedTodos });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log('App componentDidUpdate');
+
+    const nextTodos = this.state.todos;
+    const prevTodos = prevState.todos;
+
+    if (nextTodos !== prevTodos) {
+      // console.log('Обновилось поле todos, записываю todos в хранилище');
+      localStorage.setItem('todos', JSON.stringify(nextTodos));
+    }
+
+    if (nextTodos.length > prevTodos.length && prevTodos.length !== 0) {
+      this.toggleModal();
+    }
+  }
 
   addTodo = text => {
     const todo = {
@@ -28,25 +59,12 @@ class App extends Component {
   };
 
   deleteTodo = todoId => {
-    this.setState(prevState => ({
-      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    this.setState(({todos}) => ({
+      todos: todos.filter(({id}) => id !== todoId),
     }));
   };
 
   toggleCompleted = todoId => {
-    // this.setState(prevState => ({
-    //   todos: prevState.todos.map(todo => {
-    //     if (todo.id === todoId) {
-    //       return {
-    //         ...todo,
-    //         completed: !todo.completed,
-    //       };
-    //     }
-
-    //     return todo;
-    //   }),
-    // }));
-
     this.setState(({ todos }) => ({
       todos: todos.map(todo =>
         todo.id === todoId ? { ...todo, completed: !todo.completed } : todo,
@@ -62,8 +80,8 @@ class App extends Component {
     const { filter, todos } = this.state;
     const normalizedFilter = filter.toLowerCase();
 
-    return todos.filter(todo =>
-      todo.text.toLowerCase().includes(normalizedFilter),
+    return todos.filter(({text}) =>
+      text.toLowerCase().includes(normalizedFilter),
     );
   };
 
@@ -76,30 +94,44 @@ class App extends Component {
     );
   };
 
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
   render() {
-    const { todos, filter } = this.state;
+    const { todos, filter, showModal } = this.state;
     const totalTodoCount = todos.length;
     const completedTodoCount = this.calculateCompletedTodos();
     const visibleTodos = this.getVisibleTodos();
 
     return (
       <Container>
-        {/* TODO: вынести в отдельный компонент */}
+
+        <IconButton onClick={this.toggleModal} aria-label="Добавить todo">
+          <AddIcon width="40" height="40" fill="#fff" />
+        </IconButton>
+        
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <TodoEditor onSubmit={this.addTodo} />
+          </Modal>
+        )}
 
         <div>
           <p>Всего заметок: {totalTodoCount}</p>
           <p>Выполнено: {completedTodoCount}</p>
         </div>
 
-        <TodoEditor onSubmit={this.addTodo} />
-
-        <Filter value={filter} onChange={this.changeFilter} />
+        <TodoFilter value={filter} onChange={this.changeFilter} />
 
         <TodoList
           todos={visibleTodos}
           onDeleteTodo={this.deleteTodo}
           onToggleCompleted={this.toggleCompleted}
         />
+
       </Container>
     );
   }
---
+++ src/components/Todo/todo.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import IconButton from '../IconButton/iconButton';
+import { ReactComponent as DeleteIcon } from '../../icons/delete.svg';
+import classNames from 'classnames';
+
+const Todo = ({ text, completed, onToggleCompleted, onDelete }) => (
+  <>
+    <input
+      type="checkbox"
+      className="TodoList__checkbox"
+      checked={completed}
+      onChange={onToggleCompleted}
+    />
+    <p className="TodoList__text">{text}</p>
+
+    {/* <button type="button" className="TodoList__btn" onClick={onDelete}>
+      Удалить
+    </button> */}
+
+    <IconButton onClick={onDelete} aria-label="Удалить todo" className={classNames('IconButton','TodoList__iconBtn')}>
+      <DeleteIcon width='32' height='32' fill='#fff'/>
+    </IconButton>
+  </>
+);
+
+export default Todo;
