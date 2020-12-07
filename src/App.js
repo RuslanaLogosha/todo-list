ed384e4ffc2369e848a02239d372509bd88df14f
Ruslana Logosha <ruslanalogosha@gmail.com>
2020-12-07 14:34:57 +0200

    code and styles

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import shortid from 'shortid';
+
+import TodoList from './components/TodoList/todoList';
+import TodoEditor from './components/TodoEditor/todoEditor';
+import initialTodos from './components/TodoList/todos.json';
+import Filter from './components/Filter';
+import Container from './components/Container/container';
+
+
+
+class App extends Component {
+  state = {
+    todos: initialTodos,
+    filter: '',
+  };
+
+  addTodo = text => {
+    const todo = {
+      id: shortid.generate(),
+      text,
+      completed: false,
+    };
+
+    this.setState(({ todos }) => ({
+      todos: [todo, ...todos],
+    }));
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  toggleCompleted = todoId => {
+    // this.setState(prevState => ({
+    //   todos: prevState.todos.map(todo => {
+    //     if (todo.id === todoId) {
+    //       return {
+    //         ...todo,
+    //         completed: !todo.completed,
+    //       };
+    //     }
+
+    //     return todo;
+    //   }),
+    // }));
+
+    this.setState(({ todos }) => ({
+      todos: todos.map(todo =>
+        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleTodos = () => {
+    const { filter, todos } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return todos.filter(todo =>
+      todo.text.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  calculateCompletedTodos = () => {
+    const { todos } = this.state;
+
+    return todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0,
+    );
+  };
+
+  render() {
+    const { todos, filter } = this.state;
+    const totalTodoCount = todos.length;
+    const completedTodoCount = this.calculateCompletedTodos();
+    const visibleTodos = this.getVisibleTodos();
+
+    return (
+      <Container>
+        {/* TODO: вынести в отдельный компонент */}
+
+        <div>
+          <p>Всего заметок: {totalTodoCount}</p>
+          <p>Выполнено: {completedTodoCount}</p>
+        </div>
+
+        <TodoEditor onSubmit={this.addTodo} />
+
+        <Filter value={filter} onChange={this.changeFilter} />
+
+        <TodoList
+          todos={visibleTodos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Container/container.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import './container.scss';
+
+const Container = ({ children }) => <div className="Container">{children}</div>;
+
+export default Container;
